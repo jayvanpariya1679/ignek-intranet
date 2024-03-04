1bacd89b75726234d2f9d7a88b559b619cdff569
jayvanpariya1679 <jayvanpariya1679@gmail.com>
2024-03-04 10:52:22 +0530

    review resolved commit

---
+++ ignek_intranet/modules/intranet-entities-counts/src/main/java/com/ignek/intranet/counts/constants/IntranetConstant.java
@@ -0,0 +1,13 @@
+package com.ignek.intranet.counts.constants;
+
+/**
+ * @author ignek
+ */
+public class IntranetConstant {
+
+	public static final String PORLET_ID = "com_ignek_intranet_counts_IntranetEntitiesCountsPortlet";
+
+	public static final String CONFIGURATION_ID = "com.ignek.intranet.counts.config.IntranetConfig";
+
+	public static final String CARD = "card";
+}
